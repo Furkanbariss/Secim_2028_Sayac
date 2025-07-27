4978b72f255e82a1f93c03fec8a33d87c4a095e3
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-07-27 21:50:01 +0300

    add admob adverbs add calibri text style fixed some issues more responsive design

---
+++ app/src/main/java/com/furkanbarissonmezisik/secim2028sayac/AdMobBanner.kt
@@ -0,0 +1,22 @@
+package com.furkanbarissonmezisik.secim2028sayac
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+@Composable
+fun AdMobBanner(modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                adUnitId = "ca-app-pub-3940256099942544/6300978111" // 👈 Test Banner ID (geliştirirken BUNU kullan)
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
+}
